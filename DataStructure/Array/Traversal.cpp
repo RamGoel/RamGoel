5f484b85076ba23403bcfd763a038fc25be908ba
Ram Goel <rgoel766@gmail.com>
2022-03-09 12:18:42 +0530

    Update Traversal.cpp

--- DataStructure/Array/Traversal.cpp
+++ DataStructure/Array/Traversal.cpp
@@ -1,30 +1,49 @@
 /******************************************************************************
-
                               Cpp Data Structures and Algorithms
                                 Example of Array Creation in Cpp
-
 *******************************************************************************/
 
 #include <iostream>
 
 using namespace std;
 
+
+void createArray(int arr[],int arrSize){
+    for(int i =0;i<arrSize;i++){
+        cin>>arr[i];
+    }
+    
+    
+}
+void printArray(int arr[],int arrSize){
+    
+    //Array Traversal (Visiting all Elements or Printing all Elements)
+    for(int i=0;i<arrSize;i++){
+        cout<<i+1<<"th element of Array is "<<arr[i]<<endl;
+    }
+    
+}
+
 int main()
 {
     
-    int i,size; //Initializing a Traverse Variable i and size variable for size of array
     
-    //Creating an Array
+    int arrSize;
+    
+    cout<<"Enter Array Size:";
+    cin>>arrSize;
     
     //Declaration and Initialization of an array. 
-    int arr[]={23,43,56,77,67,89,90};
+    int arr[arrSize];
     
-    //Size will be equal to no. of Elements in Initialization.
-    size =sizeof(arr)/sizeof(int);
+    cout<<"Enter Array Elements: ";
+    createArray(arr,arrSize);
+    
+    //Calling the Function
+    printArray(arr,arrSize);
     
     
-    //Array Traversal (Visiting all Elements or Printing all Elements)
-    for(i=0;i<size;i++){
-        cout<<i+1<<"th element of Array is "<<arr[i]<<endl;
-    }
+    
+    
+   
 }
